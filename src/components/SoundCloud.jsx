cd14a19864d62a6685e9d95c4cb466b41911a495
Andrew Tung <atungw@gmail.com>
2016-05-17 20:00:49 -0700

    Moved api.js back to the head for consistency. Moved  the SoundCloud widget to be rendered with React so it's more "componentized"

--- src/components/SoundCloud.jsx
+++ src/components/SoundCloud.jsx
@@ -43,6 +43,7 @@ export default class SoundCloud extends React.Component {
 	render() {
 		return (
 			<div>
+				<iframe id="sc-widget" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1848538&show_artwork=true"></iframe>	
 				<h1>Search by Track Title</h1>
 				<table>
 					<thead>
